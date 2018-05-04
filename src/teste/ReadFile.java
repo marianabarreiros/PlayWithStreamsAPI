9ca81833aed9922aa9ab14ac8d65da2c80fddf98
Mariana Barreiros <maridepp1@hotmail.com>
2018-05-04 14:07:58 -0300

    first commit

---
+++ src/teste/ReadFile.java
@@ -0,0 +1,50 @@
+package teste;
+
+import java.io.File;
+import java.io.IOException;
+import static java.lang.System.exit;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ReadFile {
+
+    public boolean verificarArquivoExiste() throws URISyntaxException{
+        final String FILE = "Hotel" + File.separator;
+        boolean ret = false;
+        try{
+            Stream<Path> stream = Files.list(Paths.get(getClass().getClassLoader().getResource(FILE).toURI()));
+            Optional<Path> arq = stream.filter(p -> p.toString().endsWith("HotelReservation.txt")).findAny();
+            ret = arq.isPresent(); //informa se o arquivo está presente
+        } catch (NullPointerException ex) {
+            System.out.println("Caminho não encontrado");
+            exit(0);
+        } catch (IOException ex){
+            System.out.println("");
+        }
+        return ret;
+    }
+
+    public List<String> getListaDeEntradas()throws IOException, URISyntaxException{
+        final String FILE = "file" + File.separator + "HotelReservation.txt";
+        Path caminho = Paths.get(getClass().getClassLoader().getResource(FILE).toURI());
+        Stream<String> linhas = Files.lines(caminho, StandardCharsets.ISO_8859_1);
+        List<String> listaDeLinhas = linhas.collect(Collectors.toList());
+        return listaDeLinhas;
+//        List<Jogador> listaDeJogadores = new ArrayList<>();
+//        Jogador jogador;
+//        Iterator it = listaDeLinhas.listIterator();
+//        String str = null;
+//        while (it.hasNext()) {
+//            str = (String) it.next();
+//        }
+//        return null;
+    }
+}
---
+++ src/teste/Teste.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+
+public class Teste {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+//        System.out.println(toSplit("26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)\n26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"));
+//        ArrayList<String> string = new ArrayList<>(Arrays.asList("Regular: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)", "Regular: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"));
+//        String cliente;
+//        String datas;
+//        int endIndex;
+//        for (int i = 0; i < string.size(); i++) {
+//            endIndex = string.get(i).indexOf(":");
+//            cliente = string.get(i).substring(0, endIndex);
+//            datas = string.get(i).substring(endIndex);
+//            System.out.println(cliente);
+//            System.out.println(datas);
+//          string.forEach(System.out::println);
+//          string.stream()
+//                  .filter(predicate)
+//        }
+//        System.out.println(string);
+        ReadFile rd = new ReadFile();
+//        System.out.println(rd.verificarArquivoExiste());
+        rd.getListaDeEntradas().stream()
+               .map(s -> s.replace(" ", ""))
+               .forEach(System.out::println);
+                
+        }
+}
+    
+//    private String removeSpaces(String string) {return string.replace(" ", "").trim();}
+//
+//    private static List<String> toSplit(String string) {
+//        String[] stringSplit = null;
+//        stringSplit = string.split("\n");
+//        return convertToArrayList(stringSplit);
+//       
+//    
+//    }
+//    private static List<String> convertToArrayList(String[] dates){
+////        ArrayList<String> stringSplitList = new ArrayList<>(Arrays.asList(dates));
+//        List<String> datesList = Arrays.stream(dates).collect(Collectors.toList());
+//        return datesList  ;
+//    }
+//     
+
