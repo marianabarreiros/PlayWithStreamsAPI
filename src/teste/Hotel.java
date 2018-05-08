05fae0681fafea9a57359cd6cc26113997fcf502
Mariana Barreiros <maridepp1@hotmail.com>
2018-05-08 19:45:17 -0300

    .

--- src/teste/Hotel.java
+++ src/teste/Hotel.java
@@ -1,8 +1,12 @@
 package teste;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Hotel {
   private char classification;
   private String name;
+  private List<PriceTable> priceTable;
   
     public Hotel(char classification, String name) {
         this.classification = classification;
@@ -16,4 +20,17 @@ public class Hotel {
     public String getName() {
         return name;
     }
+    
+//    public void addTableDynamically(PriceTable table){           
+////        Stream<TabelaDePreco> precoStream = this.tabelaPreco.stream();
+//        boolean existePrecoCadastrado = precoStream.filter(p -> p.getTipoCliente().equals(tabela.getTipoCliente())).findAny().isPresent();
+//        priceTable.stream()
+//                .filter(p - > p.)
+//        
+//        if(!existePrecoCadastrado){
+//            tabelaPreco.add(tabela);
+//        }else
+//            System.out.println("Esse Cliente já foi adicionado ao hotel");    
+//    }
+    
 }
