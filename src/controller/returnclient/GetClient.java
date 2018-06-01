dce37fcd5020a5b85d7d7bfe14ace48ef5810a3b
Mariana Barreiros <maridepp1@hotmail.com>
2018-05-31 21:03:58 -0300

    tratando clientes que não existem

--- src/controller/returnclient/GetClient.java
+++ src/controller/returnclient/GetClient.java
@@ -1,14 +1,20 @@
 package controller.returnclient;
 
 import controller.simpleclientfactory.SimpleClientFactory;
+import static javafx.application.Platform.exit;
 
 public class GetClient {
 
     private SimpleClientFactory simpleClientFactory = new SimpleClientFactory();
 
     public String returnClient(String fileLine) {
-        simpleClientFactory.creatClient(getNameClient(fileLine));
-        return simpleClientFactory.getClientType().getTypeClient();
+        try {
+            if(simpleClientFactory.creatClient(getNameClient(fileLine)))
+                return simpleClientFactory.getClientType().getTypeClient();
+        } catch (NullPointerException e) {
+            System.out.println("Erro: Cliente não existe");
+        }
+        return null;
     }
 
     private String getNameClient(String fileLine) {
